107dbfa87b265558bc7f992ec0bd853db9e31955
Fatna-w <134234864+Fatna-w@users.noreply.github.com>
2023-05-28 00:51:29 +0100

    edit

--- Clients/product-description.php
+++ Clients/product-description.php
@@ -1,71 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php
-        @include_once('header.php');
-        ?>
+    <?php include_once 'header.php'; ?>
 </head>
 <body>
     <div>
+        <?php include_once 'navbar.php'; ?>
+    </div> 
 
-    <?php
-        @include_once('navbar.php');
-        ?>
-            
-            </div> 
-<div class="product1">
-<div class="description-product">
-                <h2>Product/organiser lespace autou</h2>
-            </div>
-        
-            <div class="prod1-img">
+    <div class="product1">
+        <div class="description-product">
+            <h2>Product/organiser l'espace autour</h2>
+        </div>
+
+        <div class="prod1-img">
             <?php
-            if(isset($_GET['id'])){
+            if (isset($_GET['id'])) {
                 $productId = $_GET['id'];
-                $query="SELECT * FROM products where id = $productId";
-                $result=mysqli_query($conn,$query);
-                $rowProDetails=mysqli_fetch_row($result);
-                ?>
+                $query = "SELECT * FROM products WHERE id = $productId";
+                $result = mysqli_query($conn, $query);
+                $row = mysqli_fetch_assoc($result);
+            }
+            ?>
 
-                <img src="product/ <?php echo $rowProDetails['img'];?> " alt="">
-                
-                <div class="discp-prod">
-                <h1><?php echo $rowProDetails['title'];?> </h1>
-                   <h3><?php echo $rowProDetails['prix'];?></h3>
-                   <form action="">
-                   <label for="quantity" >Quantity 
-                   </label><br>
-                <input type="number" id="quantity" name="quantity" min="1" max="300">
-                   </form>
-                   <h2></h2>
-                   <p>
-                   <?php echo $rowProDetails['description'];
-                   ?> 
-                   </p><br>
-               
-                  
-                 <a href="#">Add To Cart</a>
-                </div>
+            <img src="product/<?php echo $row['img']; ?>" alt="">
 
-
-                <?php } ?>
-
-
+            <div class="discp-prod">
+                <h1><?php echo $row['title']; ?></h1>
+                <h3><?php echo $row['prix']; ?></h3>
+                <form action="">
+                    <label for="quantity">Quantity:</label><br>
+                    <input type="number" id="quantity" name="quantity" min="1" max="300">
+                </form>
+                <h2></h2>
+                <p>
+                    <?php echo $row['description']; ?>
+                </p>
+                <br>
+                <a href="#">Add To Cart</a>
             </div>
-      
-      
-</div>
-            
-            <br>
-            <br>
-            <br>
-            <br>
+        </div>
+    </div>
 
-            <?php
-     @include_once('footer.php');
-         ?>
-    </body>
-    
-    </html>
-           
-            
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <?php include_once 'footer.php'; ?>
+</body>
+</html>
